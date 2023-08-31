3e425a9fd7e572b5f89ae590ae78df4b329599c3
Nakamura <kisy23013@kibireha.ac.jp>
2023-08-31 10:59:40 +0900

    EX3_Calc.javaを追加

---
+++ EX3_Calc/EX3_Calc.java
@@ -0,0 +1,10 @@
+public class EX3_Calc {
+    public static void main(String[] args) {
+        int a = 100, b = 30;
+        
+        System.out.println("100 * 30 = " + (a * b));
+        System.out.println("100 / 30 = " + (((double)a / b)));
+        System.out.println("100 + 30 = " + (a + b));
+        System.out.println("100 - 30 = " + (a - b));
+    }
+}
